952bf0b80780e31b07305541081b8ddd6ff0cfd6
Prashant Parate <84656682+prash15-Github@users.noreply.github.com>
2021-07-03 21:14:42 +0530

    Add files via upload

---
+++ Iam-module/Iam-policy.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
