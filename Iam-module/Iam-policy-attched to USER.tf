01cba80af8395eb3a201806a355f9ee6e2bea5c6
Prashant Parate <84656682+prash15-Github@users.noreply.github.com>
2021-07-05 21:59:46 +0530

    Delete Iam-policy-attched to USER.tf

--- Iam-module/Iam-policy-attched to USER.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_user_policy_attachment" "S3-Read-Only" {
-  name = "S3-Read-Only-Access"
-  user = [aws_iam_user.user.abhi]
-  policy_arn = arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess
-}
