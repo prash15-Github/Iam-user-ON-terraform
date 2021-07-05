b185db897a0fd4804c0892691bc29df5fb953019
Prashant Parate <84656682+prash15-Github@users.noreply.github.com>
2021-07-05 22:36:46 +0530

    Update iam user.tf

--- Iam-module/iam user.tf
+++ Iam-module/iam user.tf
@@ -3,30 +3,24 @@ provider "aws" {
   access_key = "AKIATTEZVLXIH57SWO5S"
   secret_key = "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"  
 }
-resource "aws_iam_user" "ajit" {
-  name = "ajit"
+resource "aws_iam_user" "user1" {
+  name = "user1"
 }
-resource "aws_iam_user" "anup" {
-  name = "anup"
+resource "aws_iam_user" "user2" {
+  name = "user2"
 }
-resource "aws_iam_user" "yash" {
-  name = "yash"
+resource "aws_iam_user" "user3" {
+  name = "user3"
 }
-resource "aws_iam_user" "bhushan" {
-  name = "bhushan"
+resource "aws_iam_user" "user4" {
+  name = "user4"
 }
-resource "aws_iam_user" "rukh" {
-  name = "rukh"
+resource "aws_iam_user" "user5" {
+  name = "user5"
 }
-resource "aws_iam_user" "shubham" {
-  name = "shubham"
+resource "aws_iam_user" "user6" {
+  name = "user6"
 }
-resource "aws_iam_user" "ashish" {
-  name = "ashish"
-}
-resource "aws_iam_user" "shri" {
-  name = "shri"
-}
-resource "aws_iam_user" "gaurav" {
-  name = "gaurav"
+resource "aws_iam_user" "user7" {
+  name = "user7"
 }
